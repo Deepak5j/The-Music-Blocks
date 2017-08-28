c2fe726131c7ae195217598e3b4b3beaad5634a7
deepak5j <gautamxdeepak@gmail.com>
2017-08-28 15:29:21 +0530

    The Music Blocks
    Sololearn codes.

---
+++ main.js
@@ -0,0 +1,31 @@
+var context = new (window.AudioContext || window.webkitAudioContext)();
+function message() {
+    alert("Touch colored blocks.\n\nUse ear phone for better experience.\n\nIncrease volume of your device.");
+} 
+function snd(freq,typ,i) { 
+   
+    var op = getBgColor(i);
+    
+    var array = op.match(/\d+/g);
+
+    array[0] -= 50;
+    array[1] -= 50;
+    array[2] -= 50;
+    var newCol = "rgb(" + array[0] + "," + array[1] + "," + array[2] + ")";
+ 
+    document.body.style.backgroundColor = op; 
+
+    var osc = context.createOscillator();
+    osc.type = typ;
+    osc.frequency.value = freq;
+    osc.connect(context.destination);
+    osc.start();
+    osc.stop(context.currentTime + 0.8);
+
+}
+
+function getBgColor(ii) {
+    var elem = document.getElementById(ii);
+    var theCSSprop = window.getComputedStyle(elem, null).getPropertyValue("background-color");
+    return theCSSprop;
+}
